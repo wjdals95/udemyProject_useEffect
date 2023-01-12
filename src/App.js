2c5151b1ed0840f69286dc3ece8ecd4a97a0a6cd
wjdals95 <choejeongmin@choejeongmin-ui-MacBookAir.local>
2023-01-12 16:04:49 +0900

    Context API 사용

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
+import AuthContext from "./store/auth-context";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
@@ -23,18 +24,20 @@ function App() {
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn')
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn,
+    }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
